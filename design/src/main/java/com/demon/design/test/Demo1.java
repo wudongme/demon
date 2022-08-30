92c95f44a18af22cd9f687a3061d625d9f196238
dongmo.wu <540567641@qq.com>
2022-08-30 17:58:02 +0800

    commit

---
+++ design/src/main/java/com/demon/design/test/Demo1.java
@@ -0,0 +1,14 @@
+package com.demon.design.test;
+
+/**
+ *
+ * @desc
+ * @fileName Demo1.java
+ * @date 2022/8/30/0030 17:56
+ * @author Dongmo.Wu
+ */
+public class Demo1 {
+	public static void main(String[] args) {
+		System.out.println("111");
+	}
+}
